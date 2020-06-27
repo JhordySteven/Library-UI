606f8189f5f547322e0d3730f3ca2a5f89e9a861
Jvc <jhordyvc11>
2020-06-26 22:57:05 -0500

    modificacion service

--- src/app/Service/cat-producto.service.ts
+++ src/app/Service/cat-producto.service.ts
@@ -1,21 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { Observable } from 'rxjs';
+import{RutaMvc} from './General';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CatProductoService {
   constructor(private http:HttpClient) { }
-
   obtenerCategoria(){
-    return this.http.get("http://localhost:8888/api/listarCatProducto");
+    return this.http.get(RutaMvc+"listarCatProducto");
   }
   registrarCatProducto(categoria:any):Observable<any>{
-    return this.http.post("http://localhost:8888/api/mntCatProducto",categoria,
+    return this.http.post(RutaMvc+"mntCatProducto",categoria,
     {responseType: 'text'});
   }
   updateCatProducto(categoria:any):Observable<any>{
-    return this.http.put("http://localhost:8888/api/updtCatProducto",categoria,{responseType: 'text'});
+    return this.http.put(RutaMvc+"updtCatProducto",categoria,{responseType: 'text'});
   }
 } 
